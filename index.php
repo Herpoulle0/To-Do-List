f1b207d62b65ad154825ae6836560e9d83806fd6
Romain <romainjourdan2609@gmail.com>
2023-01-12 16:37:56 +0100

    Chuis a bout, je stop la jte jure j'ai passer 80% de mon temps a chercher tester chercher tester...

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>To Do List</title>
 </head>
 <body>
-    
+    <h1>To Do List</h1>
+
+    <form method="POST">
+        Add New : <input type="text" name="chevre">
+        <input type="submit">
+    </form>
+
+
+<?php
+    echo $_POST["chevre"];
+?>
+
+
 </body>
 </html>
